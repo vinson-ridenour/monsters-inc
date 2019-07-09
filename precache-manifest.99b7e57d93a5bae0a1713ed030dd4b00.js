d46d38ca550a57ace2ddeb0e783881146888587b
Vinson Ridenour <vinson.ridenour@gmail.com>
2019-07-08 23:45:44 -0700

    Updates

---
+++ precache-manifest.99b7e57d93a5bae0a1713ed030dd4b00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2a436ca624835acb741899af511b6be",
+    "url": "/monsters-inc/index.html"
+  },
+  {
+    "revision": "4a9e15ebf0630b390713",
+    "url": "/monsters-inc/static/css/main.2ce0608c.chunk.css"
+  },
+  {
+    "revision": "6ea22432601858ce9ef5",
+    "url": "/monsters-inc/static/js/2.27e9ad4f.chunk.js"
+  },
+  {
+    "revision": "4a9e15ebf0630b390713",
+    "url": "/monsters-inc/static/js/main.71cacff6.chunk.js"
+  },
+  {
+    "revision": "ba3409997aeea374dd25",
+    "url": "/monsters-inc/static/js/runtime~main.412e6c78.js"
+  }
+]);
